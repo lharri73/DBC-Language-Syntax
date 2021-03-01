a84c48e51329c785b44403fbdc87db75b716d3f7
Landon Harris <lharri73@vols.utk.edu>
2021-02-28 22:13:12 -0500

    working iterator over all messages

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,29 +1,49 @@
 import { Component, HostListener, OnInit } from '@angular/core';
 import { reviver } from "../../../server/out/mapTools.js";
 import { Database } from "../../../server/out/dbc/db.js";
-import {Uri} from 'vscode'
+
+import { fromEvent, Observable, Subject, of } from 'rxjs';
+import { Message } from '../../../server/out/dbc/message.js';
 
 @Component({
     selector: 'app-root',
     templateUrl: './app.component.html',
     styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
 
-    private db: Database | null;
+    public msgObservable: Observable<Message[]>;
     public title: string;
+
+    private db: Database;
+    private messages: Subject<Message[]>
+    
     constructor(){
-        this.db = null;
+        this.db = new Database();
         this.title = "DBC Preview";
+        this.messages = new Subject<Message[]>();
+        this.msgObservable = this.messages.asObservable();
     }
 
-    @HostListener('window:message', ['$event'])
-    handleMessage(event: MessageEvent){
-        this.db = JSON.parse(event.data, reviver);
-        var pth = this.db.fileName;
-        console.log('here', pth);
-        var pthStr = pth.split('/');
+    // @HostListener('window:message', ['$event'])
+    // handleMessage(event: MessageEvent){
+    //     this.db = JSON.parse(event.data, reviver);
+    //     var pth = this.db.fileName;
+    //     console.log('here', pth);
+    //     var pthStr = pth.split('/');
         
-        this.title = pthStr[pthStr.length - 1];
+    //     this.title = pthStr[pthStr.length - 1];
+    // }
+
+    ngOnInit(){
+        const msg = fromEvent(window, 'message');
+        msg.subscribe((event: MessageEvent) =>{
+            var db = JSON.parse(event.data, reviver);
+            this.db = db;
+            var pth = db.fileName;
+            var pthStr = pth.split('/');
+            this.title = pthStr[pthStr.length - 1];
+            this.messages.next(db.messages.values());
+        });
     }
 }
---
+++ client/src/app/message/message.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnChanges, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-message',
+  templateUrl: './message.component.html',
+  styleUrls: ['./message.component.scss']
+})
+export class MessageComponent implements OnInit, OnChanges {
+
+  constructor() { }
+
+  ngOnInit(): void {
+
+  }
+
+  ngOnChanges(): void{
+    
+  }
+
+}
