70be61d41bfe9d1379c35dd1e1aca2a0740997b1
Landon Harris <lharri73@vols.utk.edu>
2021-03-16 23:48:51 -0400

    added basic message component

--- client/src/app/message/message.component.ts
+++ client/src/app/message/message.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnChanges, OnInit } from '@angular/core';
+import { Component, Input, OnChanges, OnInit } from '@angular/core';
+import { Message } from '../../../../server/out/dbc/message';
 
 @Component({
   selector: 'app-message',
@@ -7,10 +8,19 @@ import { Component, OnChanges, OnInit } from '@angular/core';
 })
 export class MessageComponent implements OnInit, OnChanges {
 
+  @Input()
+  private message: Message;
+
+  public decId: string;
+  public hexId: string;
+  public name: string;
+
   constructor() { }
 
   ngOnInit(): void {
-
+    this.hexId = this.message.id.toString(16).padStart(3, "000");
+    this.decId = this.message.id.toString(10).padStart(4, "0000");
+    this.name = this.message.name;
   }
 
   ngOnChanges(): void{
